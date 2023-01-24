3769eeaf826db2ea5520fedb1173369f8c865674
Hrayr <hrayr.ohanjanian@gmail.com>
2023-01-24 11:25:50 +0400

    Changed random int

--- src/main/java/utils/RandomUtils.java
+++ src/main/java/utils/RandomUtils.java
@@ -2,9 +2,12 @@ package utils;
 
 import org.apache.commons.lang3.RandomStringUtils;
 
+import java.util.Random;
+
 
 public class RandomUtils {
-    public static long randomInt = (long) (Math.random() * 10000);
+    private static Random random = new Random();
+    public static int randomInt = random.nextInt(10000);
 
     public static String randomString (int lengthOfString) {
         return RandomStringUtils.randomAlphabetic(lengthOfString).toLowerCase();
